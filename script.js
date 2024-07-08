71876c772d0baf84baabc9159e70d8d504271e50
alviansyahrazak <alviansyahrazak@gmail.com>
2024-07-09 02:59:14 +0800

    feat: making UI

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-alert("welcome, this is a simple game of rock, paper, and scissors, this game has 5 rounds of play (The game will start in 5 seconds, please open devtools and see the console for results)")
+const playerScoreSpan = document.getElementById("player_score_span")
+const computerScoreSpan = document.getElementById("computer_score_span")
+const roundCountSpan = document.getElementById("round_count_span")
+const message = document.getElementById("message")
+const rockBtn = document.getElementById("rock_btn")
+const paperBtn = document.getElementById("paper_btn")
+const scissorsBtn = document.getElementById("scissors_btn")
+const resetGameBtn = document.getElementById("reset_game_btn")
+const resetButtonWrapper = document.querySelector(".reset_button_wrapper")
+const choiceButtonsWrapper = document.querySelector(".choice_buttons_wrapper")
 
-let humanScore = 0
+let playerScore = 0
 let computerScore = 0
+let roundCount = 0
 
 const getComputerChoice = () => {
     let randomNum = Math.floor(Math.random() * 3);
@@ -14,54 +24,61 @@ const getComputerChoice = () => {
     }
 }
 
-const getHumanChoice = () => {
-    let input;
-    const choice = ["rock", "paper", "scissors"]
-    while(true) {
-        input = prompt('type one option "rock", "paper" or "scissors"');
-        if (input != null && choice.includes(input.toLowerCase()) ) {
-            return input;
-        } else {
-           alert('wrong input!, please type one option "rock", "paper" or "scissors"')
-        }
-    }
-}
-
-const playRound = (humanChoice, computerChoice) => {
-    let lowerCaseHumanChoice = humanChoice.toLowerCase()
-    if ((lowerCaseHumanChoice === "rock") && (computerChoice === "scissors")) {
-        humanScore++
-        return "You Win! Rock beats Scissors"
-    } else if ((lowerCaseHumanChoice === "paper") && (computerChoice === "rock")) {
-        humanScore++
-        return "You Win! Paper beats Rock"
-    } else if ((lowerCaseHumanChoice === "scissors") && (computerChoice === "paper")) {
-        humanScore++
-        return "You Win! Scissors beats Paper"
-    } else if ((lowerCaseHumanChoice === "scissors") && (computerChoice === "rock")) {
-        computerScore++
-        return "You Lose! Rock beats Scissors"
-    } else if ((lowerCaseHumanChoice === "rock") && (computerChoice === "paper")) {
-        computerScore++
-        return "You Lose! Paper beats Rock"
-    } else if ((lowerCaseHumanChoice === "paper") && (computerChoice === "scissors")) {
-        computerScore++
-        return "You Lose! Scissors beats Paper"
+const playRound = (playerChoice, computerChoice) => {
+    if (playerChoice === computerChoice) {
+        message.innerText = "It's a tie!";
+    } else if ((playerChoice === "rock") && (computerChoice === "scissors") || 
+        (playerChoice === "paper") && (computerChoice === "rock")  ||
+        (playerChoice === "scissors") && (computerChoice === "paper")) {
+        playerScore++
+        playerScoreSpan.innerText = `${playerScore}`
+        message.innerText = `You Win! ${playerChoice} beats ${computerChoice}`;
     } else {
-        return "It's a tie!";
+        computerScore++;
+        computerScoreSpan.innerText = `${computerScore}`;
+        message.innerText = `You Lose! ${computerChoice} beats ${playerChoice}`;
     }
 }
 
-const playGame = () => {
-    setTimeout( function() {
-    for(i = 1; i <= 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection))
-        }
-        console.log("human score" + " " + humanScore)
-        console.log("computer score" + " " + computerScore)
-    }, 5000)
+const playGame = (playerChoice) => {
+    let playerSelection = playerChoice
+    let computerSelection = getComputerChoice()
+    roundCount++
+    roundCountSpan.innerText = `${roundCount}`
+    if(roundCount <= 4) {
+        playRound(playerSelection, computerSelection)
+    } else {
+        (playerScore > computerScore) ? message.innerText = "Player has won the game!"
+        : (computerScore > playerScore) ? message.innerText = "Computer has won the game!"
+        : message.innerText = "No one wins a draw game!"
+        
+        choiceButtonsWrapper.classList.add("hidden")
+        resetButtonWrapper.classList.remove("hidden")
+    }
 }
 
-playGame()
+const resetGame = () => {
+    playerScore = 0
+    computerScore = 0
+    roundCount = 0
+    playerScoreSpan.innerText = playerScore
+    computerScoreSpan.innerText = computerScore
+    roundCountSpan.innerText = roundCount
+    message.innerHTML = ""
+    choiceButtonsWrapper.classList.remove("hidden")
+    resetButtonWrapper.classList.add("hidden")
+}
+
+
+rockBtn.addEventListener("click", () => {
+    playGame("rock")
+})
+paperBtn.addEventListener("click", () => {
+    playGame("paper")
+})
+
+scissorsBtn.addEventListener("click", () => {
+    playGame("scissors")
+})
+
+resetGameBtn.addEventListener("click", resetGame)
